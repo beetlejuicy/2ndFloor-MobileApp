a00650ca653579448979439a39a405e48fd5a9bf
rsadev <rafael.sanjos@gmail.com>
2014-10-13 19:46:52 -0300

    bug fix

--- Workspace/Ofertame/src/com/secondfloor/ofertame/BuscaOfertas.java
+++ Workspace/Ofertame/src/com/secondfloor/ofertame/BuscaOfertas.java
@@ -2,13 +2,19 @@ package com.secondfloor.ofertame;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class BuscaOfertas extends Activity {
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.);
+        setContentView(R.layout.activity_busca_ofertas);
+        
+        TextView filtroTitulo = (TextView) findViewById(R.id.txtFiltroTitulo);
+        ListView lstViewOfertas = (ListView) findViewById(R.id.lstViewOfertas);
+        
     }
 
 
