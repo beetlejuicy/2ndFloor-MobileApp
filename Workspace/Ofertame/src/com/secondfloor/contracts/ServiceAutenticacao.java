87cc1ae6cd43b16f8c4ccb10819ff8034bf5c913
rsadev <rafael.sanjos@gmail.com>
2014-10-13 17:59:10 -0300

    Inicial Layout tela 1.0 models e services basico

---
+++ Workspace/Ofertame/src/com/secondfloor/contracts/ServiceAutenticacao.java
@@ -0,0 +1,8 @@
+package com.secondfloor.contracts;
+
+import com.secondfloor.model.Autenticacao;
+
+public interface ServiceAutenticacao {
+	public boolean Cadastrar(Autenticacao autenticacao);
+	public boolean Autenticar(Autenticacao autenticacao);
+}
