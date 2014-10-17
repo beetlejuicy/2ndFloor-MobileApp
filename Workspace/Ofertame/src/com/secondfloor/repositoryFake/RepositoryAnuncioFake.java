23227666f432b0d975ad46ea31c1f7bcaef0f556
rsadev <rafael.sanjos@gmail.com>
2014-10-16 23:30:45 -0300

    Teste Fake Busca Ofertas

--- Workspace/Ofertame/src/com/secondfloor/repositoryFake/RepositoryAnuncioFake.java
+++ Workspace/Ofertame/src/com/secondfloor/repositoryFake/RepositoryAnuncioFake.java
@@ -11,6 +11,26 @@ public class RepositoryAnuncioFake implements RepositoryAnuncio{
 	@Override
 	public List<Anuncio> pesquisarAnuncioPorTitulo(String titulo) {
 		List<Anuncio> anuncios = new ArrayList<Anuncio>();
+		Anuncio a1 = new Anuncio();
+		a1.setTitulo("Monitor 28");
+		a1.setDataOferta("01/10 - 20/10");
+		a1.setClassificacaoAnunciante(4.5f);
+		a1.setAnunciante("Extra Supermercados");
+		a1.setFornecedor("Samsung");
+		a1.setPreco(500f);
+		a1.setEndereco("AV CAPITAO MARIO TOLEDO DE CAMARGO - SANTO ANDRE - SP CEP: 09400-500");
+		
+		Anuncio a2 = new Anuncio();
+		a2.setTitulo("Monitor e Tv 32");
+		a2.setDataOferta("01/10 - 20/10");
+		a2.setClassificacaoAnunciante(4.5f);
+		a2.setAnunciante("Extra Supermercados");
+		a2.setFornecedor("Samsung");
+		a2.setPreco(700f);
+		a2.setEndereco("AV CAPITAO MARIO TOLEDO DE CAMARGO - SANTO ANDRE - SP CEP: 09400-500");
+		
+		anuncios.add(a1);
+		anuncios.add(a2);
 		
 		return anuncios;
 	}
